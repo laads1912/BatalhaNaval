a5ef0a921d45586473aab3c5c8a7e0582dc6ebfe
Luiz <santosluiz1912@gmail.com>
2023-10-16 14:43:09 -0300

    Finalização Metodo atirar

--- controle/controlador_partida.py
+++ controle/controlador_partida.py
@@ -13,13 +13,9 @@ class ControladorPartida:
         self.__partida = Partida(self.__jogador, 1)
         self.__tela_partida = TelaPartida()
         self.__controlador_sistema = controlador_sistema
-        self.__tiros_realizados = []
         self.__dict_posicao = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
                                'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
                                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
-
-    def retornar(self):
-        self.__controlador_sistema.abre_tela()
 
     def iniciar_partida(self):
         while True:
